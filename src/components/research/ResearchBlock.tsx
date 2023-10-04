e5099c255be52ad36875ccf5861208db4f7cfaef
dwrx <dwrx@miso.one>
2023-10-04 22:19:52 +0200

    Add technology trees

--- src/components/research/ResearchBlock.tsx
+++ src/components/research/ResearchBlock.tsx
@@ -2,19 +2,20 @@ import React from "react";
 import "./ResearchTree.css";
 
 interface IResearch {
-  title: string;
-  description: string;
-  metadata: {
-    science_cost: number;
-  };
+  name: string;
+  cost: number;
+  unlocks: string[];
 }
 
-const ResearchBlock = ({ title, description, metadata }: IResearch) => {
+const ResearchBlock = ({ name, cost, unlocks }: IResearch) => {
   return (
     <div className="research-block">
-      <h3>{title}</h3>
-      <p>{description}</p>
-      <div className="metadata">Research points: {metadata.science_cost}</div>
+      <h3>{name}</h3>
+      <p>Unlocks: {unlocks.join(", ")}</p>
+      <div className="metadata">
+        Research points: {cost}{" "}
+        <img src="/icons/science.png" style={{ display: "inline-block", width: "24px" }} alt="" />
+      </div>
     </div>
   );
 };
